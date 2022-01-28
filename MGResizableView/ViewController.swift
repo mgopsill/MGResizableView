bf7b40946df17a4549c0f661f15bd798fe78f71c
Mike Gopsill <m.gopsill@gmail.com>
2022-01-28 16:53:46 +0000

    Initial Commit

---
+++ MGResizableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MGResizableView
+//
+//  Created by Mike Gopsill on 28/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
